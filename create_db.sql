49da269672284024776f0e2ba3faad0c9a0a6051
Zachary Dimmick <zdimmick8878@floridapoly.edu>
2026-04-17 07:13:25 -0400

    Fixed names of tables to match current

--- create_db.sql
+++ create_db.sql
@@ -3,7 +3,7 @@ BEGIN
     FOR t IN (SELECT table_name FROM user_tables 
               WHERE table_name IN (
                 'TRANSACTIONS','CARDHOLDER_LOCATION','CARDHOLDER',
-                'MERCHANT','MERCHANT_CATEGORY','CITY_DETAILS',
+                'MERCHANT','CATEGORY','CITIES',
                 'STATES','OCCUPATION'
               ))
     LOOP
@@ -23,8 +23,8 @@ CREATE TABLE cardholder (
     last_name VARCHAR2(50) NOT NULL,
     dob DATE NOT NULL,
     occupation_id NUMBER NOT NULL,
-    gender VARCHAR2(10) NOT NULL
-        CHECK (gender IN ('Male','Female','Nonbinary','Other')),
+    gender VARCHAR2(2) NOT NULL
+        CHECK (gender IN ('M','F','NB','O')),
     CONSTRAINT fk_cardholder_occupation
         FOREIGN KEY (occupation_id) REFERENCES occupation(occupation_id)
 );
@@ -34,54 +34,54 @@ CREATE TABLE states (
     state_name VARCHAR2(50) NOT NULL
 );
 
-CREATE TABLE city_details (
+CREATE TABLE cities (
     city_id NUMBER PRIMARY KEY,
     city VARCHAR2(50) NOT NULL,
     state_code CHAR(2) NOT NULL,
-    zip_code VARCHAR2(10) NOT NULL,
     city_pop NUMBER(8,0) NOT NULL,
     CONSTRAINT fk_city_state
         FOREIGN KEY (state_code) REFERENCES states(state_code)
 );
 
+
 CREATE TABLE cardholder_location (
     location_id NUMBER PRIMARY KEY,
-    cardholder_id NUMBER,
+    cardholder_id NUMBER NOT NULL,
     street VARCHAR2(50) NOT NULL,
     city_id NUMBER NOT NULL,
+    zip_code VARCHAR2(10) NOT NULL,
     latitude NUMBER(10,8) NOT NULL,
     longitude NUMBER(11,8) NOT NULL,
     CONSTRAINT fk_location_cardholder
         FOREIGN KEY (cardholder_id) REFERENCES cardholder(cardholder_id),
     CONSTRAINT fk_location_city
-        FOREIGN KEY (city_id) REFERENCES city_details(city_id)
+        FOREIGN KEY (city_id) REFERENCES cities(city_id)
 );
 
-CREATE TABLE merchant_category (
-    merchant_cat_id NUMBER PRIMARY KEY,
+CREATE TABLE category (
+    category_id NUMBER PRIMARY KEY,
     category_name VARCHAR2(32) NOT NULL
 );
 
 CREATE TABLE merchant (
     merchant_id NUMBER PRIMARY KEY,
-    merchant_name VARCHAR2(50) NOT NULL,
-    merchant_cat_id NUMBER NOT NULL,
-    merchant_lat NUMBER(10,8) NOT NULL,
-    merchant_long NUMBER(11,8) NOT NULL,
-    CONSTRAINT fk_merchant_category
-        FOREIGN KEY (merchant_cat_id) REFERENCES merchant_category(merchant_cat_id)
+    merchant_name VARCHAR2(50) NOT NULL
 );
 
 CREATE TABLE transactions (
     transaction_id NUMBER PRIMARY KEY,
     cardholder_id NUMBER NOT NULL,
     merchant_id NUMBER NOT NULL,
-    transaction_date DATE NOT NULL,
-    amount NUMBER(6,2) CHECK (amount >= 0),
+    category_id NUMBER NOT NULL,
+    transaction_lat     NUMBER(10,7)    NOT NULL,
+    transaction_long    NUMBER(11,7)    NOT NULL,
+    amount NUMBER(15,2) CHECK (amount >= 0),
     unix_time NUMBER NOT NULL,
     is_fraud NUMBER(1) CHECK (is_fraud IN (0, 1)),
     CONSTRAINT fk_txn_cardholder
         FOREIGN KEY (cardholder_id) REFERENCES cardholder(cardholder_id),
     CONSTRAINT fk_txn_merchant
-        FOREIGN KEY (merchant_id)   REFERENCES merchant(merchant_id)
+        FOREIGN KEY (merchant_id)   REFERENCES merchant(merchant_id),
+    CONSTRAINT fk_txn_category
+        FOREIGN KEY (category_id) REFERENCES category(category_id)
 );
